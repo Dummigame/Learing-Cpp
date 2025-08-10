9d82fe576779499dbcd3871085da4aad812c8885
dummigame <dummigame@duck.com>
2025-08-09 22:39:04 -0400

    Starting calculation (help me...)

--- Calc/calcadv.cpp
+++ Calc/calcadv.cpp
@@ -25,18 +25,43 @@ double calculation(std::string equation)
         MUL = '*',
         DIV = '/'
     };
-    
-    for(int i = equation.length(); i > 0; --i)
-    {
-        
-    }
 
+    std::string numbers;
+    int A = 1;
+    int s = 1;
+    int m = 1;
+    int d = 1;
+    
+    for(int i = 0; i < equation.length(); --i)
+    {
+
+        switch(equation[i])
+            {
+            case ADD: numbers.push_back(' ');
+            A++;
+            if(!a % 2)
+            {
+                add(a, b);
+            }
+
+
+            case SUB: numbers.push_back(' '); s++;
+
+            case MUL: numbers.push_back(' '); m++;
+
+            case DIV: numbers.push_back(' '); d++;
+
+            default: numbers.push_back(equation[i]);
+            }
+    }
+    double final = 0;
     return final;
 }
 
 double add(double a, double b)
 {
-    a+=b;
+    double c = a+b;
+
     return a;
 }
 
@@ -48,12 +73,12 @@ double sub(double a, double b)
 
 double mul(double a, double b)
 {
-    a=a*b;
+    a*=b;
     return a;
 }
 
 double div(double a, double b)
 {
-    a=a/b;
+    a/=b;
     return a;
 }
